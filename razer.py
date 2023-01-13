a43f7c49cf6b127056b61cd698f911db65268ee3
Sam Wilkie <sam.wilkie2903@gmail.com>
2023-01-13 00:07:05 +0000

    added animation to razer module

--- razer.py
+++ razer.py
@@ -1,6 +1,8 @@
 from ChromaPython import ChromaApp, ChromaAppInfo, ChromaGrid
 from time import sleep
+import threading
 
+animationPaused = True
 Info = ChromaAppInfo()
 Info.DeveloperName = "Sam Wilkie"
 Info.DeveloperContact = "sam.wilkie2903@gmail.com"
@@ -13,6 +15,9 @@ sleep(1)
 
 
 def set(pixels: list, height: int, width: int):
+    global animationPaused
+    animationPaused = True
+
     keyboardGrid = ChromaGrid("Keyboard")
     mouseGrid = ChromaGrid("Mouse")
     chromaLinkGrid = ChromaGrid("ChromaLink")
@@ -35,3 +40,50 @@ def set(pixels: list, height: int, width: int):
     App.Mouse.applyGrid()
     App.ChromaLink.setCustomGrid(chromaLinkGrid)
     App.ChromaLink.applyGrid()
+
+    animationPaused = False
+
+
+def animate(pattern: int):
+    global animationPaused
+    keyboardGrid = ChromaGrid("Keyboard")
+    while True:
+        if not animationPaused:
+            lights = []
+            for row in App.Keyboard._ColorGrid:
+                currentRow = []
+                for key in row:
+                    currentRow.append(key.getRGB())
+                lights.append(currentRow)
+            
+            match pattern:
+                case 1:  # pattern 1: move key lights between rows individually
+                    for index, row in enumerate(lights):
+                        lights[index].insert(1, lights[index-1][-5])
+                    for index, row in enumerate(lights):
+                        lights[index].pop(-5)
+
+                case 2:  # pattern 2: rows move
+                    lights.insert(0, lights.pop(-1))
+
+                case 3:  # pattern 3: columns move
+                    for index, row in enumerate(lights):
+                        lights[index].insert(1, lights[index].pop(-5))
+
+            for x in range(0, 6):
+                for y in range(0, 22):
+                    keyboardGrid[x][y].set(*lights[x][y])
+            App.Keyboard.setCustomGrid(keyboardGrid)
+            App.Keyboard.applyGrid()
+
+            mouseGrid = ChromaGrid("Mouse")
+            mouseGrid[2][3].set(*lights[3][2])
+            mouseGrid[7][3].set(*lights[3][7])
+
+            App.Mouse.setCustomGrid(mouseGrid)
+            App.Mouse.applyGrid()
+        sleep(0.12)
+
+
+animation = threading.Thread(target=animate, args=(3,), daemon=True)
+animation.start()
