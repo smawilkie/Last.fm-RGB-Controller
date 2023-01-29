db7b8b13838a1baf38e0d018e78481c967a5e3ff
Sam Wilkie <sam.wilkie2903@gmail.com>
2023-01-29 21:24:13 +0000

    removed unused methods

--- steelseries.py
+++ steelseries.py
@@ -1,3 +1,5 @@
+# https://github.com/slattery-mark/SteelSeries-CKL-App
+
 from json import load
 from os import getenv
 import threading
@@ -12,8 +14,6 @@ class CKL:
         self.game = 'CUSTOM_KEYBOARD_LIGHTING'
         self.game_display_name = 'Custom Keyboard Lighting'
         self.event = 'BITMAP_EVENT'
-
-        self.bitmap = [[0,0,0] for i in range(132)]
 
         self.outline = [[[0, 0, 0] for i in range(15)] for j in range(6)]
 
@@ -45,10 +45,6 @@ class CKL:
             ]
         }
         post(endpoint, json=payload)        
-        
-    def getGameDisplayName(self):
-        """Returns the name displayed in SteelSeries Engine for this application."""
-        return self.game_display_name
 
     def registerGame(self):
         """Registers this application to Engine."""
@@ -60,64 +56,17 @@ class CKL:
         }
         post(endpoint, json=payload)
 
-    def removeGame(self):
-        """Removes this application from Engine."""
-        endpoint = f'{self.sseAddress}/remove_game'
-        payload = {
-            "game": self.game
-        }
-        post(endpoint, json=payload)
-
-    def removeGameEvent(self):
-        """Removes a lighting event from Engine."""
-        endpoint = f'{self.sseAddress}/remove_game_event'
-        payload = {
-            "game": self.game,
-            "event" : self.event
-        }
-        post(endpoint, json=payload)
-
-    def sendHeartbeat(self):
-        """Sends an empty lighting event to Engine to prevent timeout."""
-        endpoint = f'{self.sseAddress}/game_heartbeat'
-        payload = {
-                "game": self.game
-        }
-        post(endpoint, json=payload)
-
-    def sendGameEvent(self, args):
-        """Sends a lighting event/frame to Engine."""
-        endpoint = f'{self.sseAddress}/game_event'
-        frame = [[0, 0, 0] for i in range(132)]
-        payload = {
-            "game": self.game,
-            "event": self.event,
-            "data" : {
-                "value" : 100,
-                "frame" : {
-                    "bitmap" : frame
-                }
-            }
-        }
-
-        with Session() as s:
-            for key in self.keys:
-                print(key)
-                frame[key] = [0, 255, 0]
-                s.post(endpoint, json=payload)
-                sleep(0.01)
-
     def setLightingArray(self):
         endpoint = f'{self.sseAddress}/game_event'
         frame = [[0, 0, 0] for i in range(132)]
-
-        usedColours = [self.outline[0][0], self.outline[0][1], self.outline[0][2], self.outline[0][3], self.outline[0][4], self.outline[0][5], self.outline[0][6], self.outline[0][7], self.outline[0][8], self.outline[0][9], self.outline[0][10], self.outline[0][11], self.outline[0][12], self.outline[0][13],
-                       self.outline[1][0], self.outline[1][1], self.outline[1][2], self.outline[1][3], self.outline[1][4], self.outline[1][5], self.outline[1][6], self.outline[1][7], self.outline[1][8], self.outline[1][9], self.outline[1][10], self.outline[1][11], self.outline[1][12], self.outline[1][13], self.outline[1][14],
-                       self.outline[2][0], self.outline[2][1], self.outline[2][2], self.outline[2][3], self.outline[2][4], self.outline[2][5], self.outline[2][6], self.outline[2][7], self.outline[2][8], self.outline[2][9], self.outline[2][10], self.outline[2][11], self.outline[2][12], self.outline[2][13], self.outline[2][14],
-                       self.outline[3][0], self.outline[3][1], self.outline[3][2], self.outline[3][3], self.outline[3][4], self.outline[3][5], self.outline[3][6], self.outline[3][7], self.outline[3][8], self.outline[3][9], self.outline[3][10], self.outline[3][11], self.outline[3][13], self.outline[3][14],
-                       self.outline[4][0], self.outline[4][1], self.outline[4][2], self.outline[4][3], self.outline[4][4], self.outline[4][5], self.outline[4][6], self.outline[4][7], self.outline[4][8], self.outline[4][9], self.outline[4][10], self.outline[4][11], self.outline[4][13], self.outline[4][14],
-                       self.outline[5][0], self.outline[5][1], self.outline[5][2], self.outline[5][5], self.outline[5][6], self.outline[5][7], self.outline[5][8], self.outline[5][9], self.outline[5][12], self.outline[5][13], self.outline[5][14]]
                
+        usedColours = [self.outline[0][i] for i in range(14)] + \
+                      [self.outline[1][i] for i in range(15)] + \
+                      [self.outline[2][i] for i in range(15)] + \
+                      [self.outline[3][i] for i in range(12)] + [self.outline[3][i] for i in range(13, 15)] + \
+                      [self.outline[4][i] for i in range(12)] + [self.outline[4][i] for i in range(13, 15)] + \
+                      [self.outline[5][i] for i in range(0, 3)] + [self.outline[5][i] for i in range(5, 10)] + [self.outline[5][i] for i in range(12, 15)]
+
         for index, key in enumerate(self.keys):
             frame[key] = [*usedColours[index]]
 
